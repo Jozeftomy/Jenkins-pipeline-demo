8b0923c9431accf8f4bbd78370a86d67f656c261
Joseph <joseph.tomy@mitz.ai>
2025-11-19 14:03:11 +0530

    intial commit

---
+++ backend/src/main/java/com/demo/SimpleApp.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        System.out.println("Backend is running!");
+        System.out.println("Server started on port 8080");
+    }
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div>
+        <h1>Frontend is Running!</h1>
+      </div>
+    );
+  }
+  
+  export default App;
+  
